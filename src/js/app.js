afcdfdcd13849b2ffefc0a3fb61c0a4f7c383ffe
E.S <extrin13@gmail.com>
2019-03-27 23:49:10 +0300

    feat: Switch on strict mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,16 @@
 function compare_numbers(value_1, value_2) {
+  "use strict";
   return value_1 - value_2;
 }
 
 function get_sorted_scores(scores) {
+  "use strict";
   let sorted_scores = scores;
   return sorted_scores.sort(compare_numbers).reverse();
 }
 
 function get_best_score(scores) {
+  "use strict";
   if (Array.isArray(scores)) {
     if (!scores.some(isNaN)) {
       return get_sorted_scores(scores)[0];
@@ -22,6 +25,7 @@ function get_best_score(scores) {
 }
 
 function get_average_from_top_3(scores) {
+  "use strict";
   if (Array.isArray(scores)) {
     if (!scores.some(isNaN)) {
       const top_3 = get_sorted_scores(scores).slice(0, 3);
